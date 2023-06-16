35a73f3cf04c47009dabc48bbc11f29c8db0bacf
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-16 16:00:37 +0700

    https://vjudge.net/problem/CodeChef-SEMCOURSES

---
+++ CodeChef-SEMCOURSES.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll t,a,b,c;
+void Solve(){
+    cin>>a>>b>>c;
+    cout<<a*b*c<<endl;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>t;
+    while (t--) Solve();
+}

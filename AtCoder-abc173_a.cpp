c1704e71d926aee81ca0f9bab62c017dc125622e
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-17 00:02:15 +0700

    https://vjudge.net/problem/AtCoder-abc173_a

---
+++ AtCoder-abc173_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll n;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    cout<<(1000-n%1000)%1000;
+}

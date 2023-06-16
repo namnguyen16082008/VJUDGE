bb69cf67f10b3b653fe88727296cedadbe612685
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-16 15:52:17 +0700

    https://vjudge.net/problem/DMOJ-ccc09s1

---
+++ DMOJ-ccc09s1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll a,b,ans=0;
+unordered_map <ll,ll> mp;
+ll Cp(ll n){
+    ll x=sqrt(n);
+    return ((x*x==n)+1)%2;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b;
+    for (ll i=sqrt(a)+Cp(a);i<=sqrt(b);i++) mp[i*i]++;
+    for (ll i=cbrt(a)+Cp(a);i<=cbrt(b);i++) ans+=mp[i*i*i]==1;
+    cout<<ans;
+}

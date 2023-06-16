314e318967a0211c764a2fd1ce61c77a90f12c73
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-16 15:26:03 +0700

    https://vjudge.net/problem/AtCoder-abc169_a

---
+++ AtCoder-abc169_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll a,b;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b;
+    cout<<a*b;
+}

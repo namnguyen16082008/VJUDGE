15aaa6650174c92940aec6c4909992f8c7182308
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-17 00:22:30 +0700

    https://vjudge.net/problem/CodeForces-50A

---
+++ CodeForces-50A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll n,m;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n>>m;
+    cout<<n*m/2;
+}

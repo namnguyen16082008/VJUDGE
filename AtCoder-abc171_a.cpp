9ef7d1c8c44fbe58cdf83f81d706eb02b44821c1
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-17 00:08:35 +0700

    https://vjudge.net/problem/AtCoder-abc171_a

---
+++ AtCoder-abc171_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+char k;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>k;
+    (k>='a' && k<='z') ? cout<<'a' : cout<<'A';
+}

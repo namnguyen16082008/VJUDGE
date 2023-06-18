acd57b1784930441797419fca99c79f94c65fec0
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-18 16:44:38 +0700

    https://vjudge.net/problem/AtCoder-arc104_a

---
+++ AtCoder-arc104_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll x,y;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>x>>y;
+    cout<<(x+y)/2<<" "<<(x-y)/2;
+}

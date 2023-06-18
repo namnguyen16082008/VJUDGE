a2a67f76d5ff53942859bbc128035e4b9c6144d1
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-18 12:48:22 +0700

    https://vjudge.net/problem/AtCoder-abc218_b

---
+++ AtCoder-abc218_b.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll n;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    while (cin>>n) cout<<char(n+96);
+}

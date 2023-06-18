ccb221a2f5aa95857d2c2c1390513db7ce6fd1da
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-18 14:24:31 +0700

    https://vjudge.net/problem/AtCoder-abc042_a

---
+++ AtCoder-abc042_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll a,b,c;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b>>c;
+    (a+b+c==17) ? cout<<"YES" : cout<<"NO";
+}

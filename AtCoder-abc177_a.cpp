b5d5906fa375913fa0a10424e7ad93efd8ab86bc
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-18 11:25:10 +0700

    https://vjudge.net/problem/AtCoder-abc177_a

---
+++ AtCoder-abc177_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll a,b,c;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b>>c;
+    (b*c>=a) ? cout<<"Yes" : cout<<"No";
+}

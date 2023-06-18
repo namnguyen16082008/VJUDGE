3dbfafb1bdae8db4885bfa6151611dfd55a2352a
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-18 15:09:25 +0700

    https://vjudge.net/problem/AtCoder-abc150_a

---
+++ AtCoder-abc150_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll k,x;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>k>>x;
+    (k*500>=x) ? cout<<"Yes" : cout<<"No";
+}

234ad0e65e182d238b07af0d3afde08bf40d74a1
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-21 11:21:33 +0700

    https://vjudge.net/problem/AtCoder-abc218_a

---
+++ AtCoder-abc218_a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll n;
+string s;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n>>s;
+    (s[n-1]=='o') ? cout<<"Yes" : cout<<"No";
+}

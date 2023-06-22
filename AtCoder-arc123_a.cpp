3baf34e123a4da95485ce605394d744d1f3b7f4c
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-22 21:36:12 +0700

    https://vjudge.net/problem/AtCoder-arc123_a

--- AtCoder-arc123_a.cpp
+++ AtCoder-arc123_a.cpp
@@ -8,7 +8,6 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin>>a>>b>>c;
-    if (a>c) swap(a,c);
     if (b*2>a+c) cout<<b*2-a-c;
     else cout<<(a+c)/2-b+2*((a+c)%2);
 }

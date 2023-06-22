cb38e50327ae89b288d2054eeae6f0d1927a2aeb
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-22 21:35:31 +0700

    https://vjudge.net/problem/AtCoder-arc123_a

---
+++ AtCoder-arc123_a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll a,b,c;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b>>c;
+    if (a>c) swap(a,c);
+    if (b*2>a+c) cout<<b*2-a-c;
+    else cout<<(a+c)/2-b+2*((a+c)%2);
+}

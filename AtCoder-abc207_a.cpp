ff601bf1d2d2e6f5aa029d6954c9b55c7100ed3a
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-24 15:31:08 +0700

    https://vjudge.net/problem/AtCoder-abc207_a

---
+++ AtCoder-abc207_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll a,b,c;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>a>>b>>c;
+    cout<<a+b+c-min(a,min(b,c));
+}

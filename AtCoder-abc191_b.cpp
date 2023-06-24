b27d3a16688024a252bc5d6bd262fe9a5a62069b
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-24 15:13:44 +0700

    https://vjudge.net/problem/AtCoder-abc191_b

--- AtCoder-abc191_b.cpp
+++ AtCoder-abc191_b.cpp
@@ -9,7 +9,8 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin>>n>>x;
-    for (ll i=1;i<=n;i++) cin>>a[i];
-    for (ll i=1;i<=n;i++)
+    for (ll i=1;i<=n;i++){
+        cin>>a[i];
         if (a[i]!=x) cout<<a[i]<<" ";
+    }
 }

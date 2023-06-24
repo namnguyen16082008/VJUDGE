2a1ce663bcf40ad98ad2ba3fe7cbbc45b31caee6
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-24 14:50:28 +0700

    https://vjudge.net/problem/AtCoder-abc215_a

---
+++ AtCoder-abc215_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+string s;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    getline(cin,s);
+    (s=="Hello,World!") ? cout<<"AC" : cout<<"WA";
+}

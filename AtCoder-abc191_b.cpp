4f358d65d673ab6f8d7ca862ee20c0f912326de9
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-24 15:12:52 +0700

    https://vjudge.net/problem/AtCoder-abc191_b

---
+++ AtCoder-abc191_b.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+const ll N=100000;
+ll n,x,a[N+5];
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n>>x;
+    for (ll i=1;i<=n;i++) cin>>a[i];
+    for (ll i=1;i<=n;i++)
+        if (a[i]!=x) cout<<a[i]<<" ";
+}

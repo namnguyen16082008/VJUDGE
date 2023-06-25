21ea4e787fe76340d40f0dc66d19dd6306cb4f4e
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-25 14:16:36 +0700

    https://vjudge.net/problem/CodeForces-1351A

---
+++ CodeForces-1351A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll t,a,b;
+void Solve(){
+    cin>>a>>b;
+    cout<<a+b<<endl;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>t;
+    while (t--) Solve();
+}

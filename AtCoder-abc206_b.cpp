2328776d5d7b8208447821a96962b9271df35b24
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-27 17:04:44 +0700

    https://vjudge.net/problem/AtCoder-abc206_b

---
+++ AtCoder-abc206_b.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll n,x;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    x=sqrt(2*n);
+    cout<<x+(x*(x+1)/2<n);
+}
